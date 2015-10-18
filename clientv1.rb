8b42a1ebe1e95b0446f2b794892ad4388123117d
Dan Turner <dturner@tcd.ie>
2015-10-18 21:45:13 +0100

    Added a more usefull client Sever now accepts connections by first adding them to a queue (thread safe) queue has a maximum after which connections are refused TODO: add control flow for different messages implement pooling system with max threads add compile.sh, README and start.sh

--- clientv1.rb
+++ clientv1.rb
@@ -1,9 +1,21 @@
 require 'socket'
 
-s = TCPSocket.new 'localhost', 3000
 
-while line = s.gets # Read lines from socket
-  puts line         # and print them
-end
+s1 = TCPSocket.new 'localhost', 3000
+s2 = TCPSocket.new 'localhost', 3000
+s3 = TCPSocket.new 'localhost', 3000
+s4 = TCPSocket.new 'localhost', 3000
+s5 = TCPSocket.new 'localhost', 3000
+s6 = TCPSocket.new 'localhost', 3000
+s7 = TCPSocket.new 'localhost', 3000
+s8 = TCPSocket.new 'localhost', 3000
 
-s.close             # close socket when done
+s1.sendmsg("here1\n")
+s2.sendmsg("here2\n")
+s3.sendmsg("here3\n")
+s4.sendmsg("here4\n")
+s5.sendmsg("here5\n")
+s6.sendmsg("here6\n")
+s7.sendmsg("here7\n")
+s8.sendmsg("here8\n")
+
--- server.rb
+++ server.rb
@@ -1,23 +1,47 @@
 require 'thread'
 require 'socket'
 
-ARGV.each do|a|
-  puts "Argument: #{a}"
-  if ARGV[0] == nil
-  	puts "No Port Specified exiting"
-  	return -1
+class SocketServer
+
+	def initialize(port)
+		@server = TCPServer.new port
+    @que = Queue.new
+    @max = 20
+	end
+
+	def run
+		while true
+      if @que.length == @max
+        @server.reject
+      else
+        @que.push(@server.accept)
+			  Thread.start (@que.pop(false)) do |newClient|
+          #for each new client do something here
+          msg = Thread.current
+          msg = msg.to_s
+          msg = msg << ": " << newClient.readline
+          puts msg
+        end
+      end
+    end
   end
+	
+	def connection
+		# do something here with the thread
+	end
+
+end
+
+ARGV.each do |a|
+	puts "Argument: #{a}"
+	if ARGV[0] == nil
+		puts "No Port Specified exiting"
+		return -1
+	end
 end
 
 port = ARGV[0]
-server = TCPServer.new port 
 puts "Using Port Number #{port}"
+server = SocketServer.new(port)
+server.run
 
-clientNumber = 0
-while true
-	client = server.accept
-	client.puts "Your are client #{clientNumber}"
-	clientNumber += 1
-	client.close
-end
-	
