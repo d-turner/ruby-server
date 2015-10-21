2c93b3c2f73d33269c2741850de887aee3613774
Daniel Turner <dturner@tcd.ie>
2015-10-21 02:55:27 +0100

    Fixed server to use a thread pool
    TODO:
    add control flow for different messages
    add README
     limit number of incoming connections

--- clientv1.rb
+++ clientv1.rb
@@ -1,31 +1,10 @@
 require 'socket'
+require 'thread'
 
-s1 = TCPSocket.new 'localhost', 3000
-s2 = TCPSocket.new 'localhost', 3000
-s3 = TCPSocket.new 'localhost', 3000
-s4 = TCPSocket.new 'localhost', 3000
-s5 = TCPSocket.new 'localhost', 3000
-s6 = TCPSocket.new 'localhost', 3000
-s7 = TCPSocket.new 'localhost', 3000
-s8 = TCPSocket.new 'localhost', 3000
+x = 0
+while true
+  soc = TCPSocket.new 'localhost', 3000
+  soc.sendmsg("Here #{x}\n")
+  x = x + 1
+end
 
-s1.sendmsg("here1\n")
-s2.sendmsg("here2\n")
-s3.sendmsg("here3\n")
-s4.sendmsg("here4\n")
-s5.sendmsg("here5\n")
-s6.sendmsg("here6\n")
-s7.sendmsg("here7\n")
-s8.sendmsg("here8\n")
-<<<<<<< HEAD
-=======
-
-s1.close
-s2.close
-s3.close
-s4.close
-s5.close
-s6.close
-s7.close
-s8.close
->>>>>>> Closed connections on client side
--- server.rb
+++ server.rb
@@ -3,36 +3,64 @@ require 'socket'
 
 class SocketServer
 
-def initialize(port)
-  @server = TCPServer.new port
-  @que = Queue.new
-  @max = 20
-end
+  def initialize(port)
+    @server = TCPServer.new port
+    @max_threads = 4
+    @que = Queue.new
+    @max = 2
+  end
 
-def run
-  while true
-    if @que.length == @max
-      @server.reject
-    else
-      @que.push(@server.accept)
-    end
-    Thread.start (@que.pop(false)) do |newClient|
-      #for each new client do something here
-      msg = Thread.current
-      msg = msg.to_s
-      msg = msg << ": " << newClient.readline
-      puts msg
-    end
+  def run
+    x = Thread.new {
+      @server.listen(@max)
+      while true
+        if @que.length < @max
+          @que.push(@server.accept)
+        end
+      end
+    }
+    workers = (0...@max_threads).map {|i|
+      Thread.new do
+        begin
+          while true
+            sleeps = 3
+            client = @que.pop(false)
+            msg = 'Thread ' << i.to_s
+            msg << ': ' << client.readline
+
+            puts msg
+            sleep sleeps
+            client.close
+          end
+        rescue ThreadError
+        end
+      end
+    }
+    workers.map(&:join)
+    x.join
+      # @workers.each {|thr|
+      #   if thr.status == 'sleep'
+      #     thr.start(@que.pop(false)) { |newClient|
+      #       #for each new client do something here
+      #       msg = Thread.current
+      #       msg = msg.to_s
+      #       msg = msg << ': ' << newClient.readline
+      #       puts msg
+      #       Thread.sleep
+      #     }
+      #   end
+      # }
+  end
+
+  def connection
+
   end
 end
 
-end
-
-port = 3001
+port = 3000
 if ARGV[0] == nil
   puts "No Port Specified using default 3001"
 else
-  puts "here"
   port = ARGV[0]
 end
 
